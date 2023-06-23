9404987fb59ff16a4bb67fb3c59f127569d21cab
auspiciousdev99 <felixleung0307@gmail.com>
2023-06-23 03:21:41 -0400

    #1 update script

--- start.bat
+++ start.bat
@@ -1,4 +1,30 @@
-start "" node new_cleverpuppet.js %1 no
+start "" node new_cleverpuppet.js %1 no 0
 timeout /t 5 /nobreak > NUL
-start "" node new_cleverpuppet.js %1 no
+start "" node new_cleverpuppet.js %1 no 1
+timeout /t 5 /nobreak > NUL
+start "" node new_cleverpuppet.js %1 no 2
+timeout /t 5 /nobreak > NUL
+start "" node new_cleverpuppet.js %1 no 0
+timeout /t 5 /nobreak > NUL
+start "" node new_cleverpuppet.js %1 no 1
+timeout /t 5 /nobreak > NUL
+start "" node new_cleverpuppet.js %1 no 2
+timeout /t 5 /nobreak > NUL
+start "" node new_cleverpuppet.js %1 no 0
+timeout /t 5 /nobreak > NUL
+start "" node new_cleverpuppet.js %1 no 1
+timeout /t 5 /nobreak > NUL
+start "" node new_cleverpuppet.js %1 no 2
+timeout /t 5 /nobreak > NUL
+start "" node new_cleverpuppet.js %1 no 0
+timeout /t 5 /nobreak > NUL
+start "" node new_cleverpuppet.js %1 no 1
+timeout /t 5 /nobreak > NUL
+start "" node new_cleverpuppet.js %1 no 2
+timeout /t 5 /nobreak > NUL
+start "" node new_cleverpuppet.js %1 no 0
+timeout /t 5 /nobreak > NUL
+start "" node new_cleverpuppet.js %1 no 1
+timeout /t 5 /nobreak > NUL
+start "" node new_cleverpuppet.js %1 no 2
 timeout /t 5 /nobreak > NUL
