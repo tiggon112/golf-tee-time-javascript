3b2228a173f9206e2a52f4a4dd6fda190becfff2
Felix Leung <felixleung0307@gmail.com>
2023-08-12 08:44:45 -0400

    updated  bot for bypassing cloudflare.

--- utils.js
+++ utils.js
@@ -1,3 +1,38 @@
+function shuffle(array) {
+  let currentIndex = array.length,
+    randomIndex;
+
+  // While there remain elements to shuffle.
+  while (currentIndex != 0) {
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+
+  return array;
+}
+
+function getReserveDate(days) {
+  const currentDate = new Date();
+  const thisWeekend = new Date(
+    currentDate.setDate(
+      currentDate.getDate() + parseInt(days)
+    )
+  );
+
+  const year = thisWeekend.getFullYear();
+  const month = (thisWeekend.getMonth() + 1).toString().padStart(2, "0");
+  const day = thisWeekend.getDate().toString().padStart(2, "0");
+
+  return `${month}/${day}/${year}`;
+}
+
 const convertTime12to24 = (time12h) => {
   const [time, modifier] = time12h.split(" ");
 
@@ -22,4 +57,4 @@ const convertLATime = (date) => {
   );
 };
 
-module.exports = { convertLATime };
+module.exports = { convertLATime, shuffle, getReserveDate };
